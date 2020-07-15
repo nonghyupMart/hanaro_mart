0169651604a7a44cfa2dd94bc8ea09ad078a3dee
Dominic Kim <dominickim@MacBook-Pro.local>
2020-07-15 15:05:35 +0900

    setup drawer menu

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>123Open up App.js to start working on your app!123123</Text>
+      <StatusBar style="auto" />
+      <Button title="change text" />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
