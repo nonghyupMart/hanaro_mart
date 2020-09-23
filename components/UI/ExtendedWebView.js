043485a26550ec2ce3971fe0f462bfb603d36e87
Dominic Kim <dominickim@MacBook-Pro.local>
2020-09-23 10:20:17 +0900

    웹뷰   alert bridge 개발

--- components/UI/ExtendedWebView.js
+++ components/UI/ExtendedWebView.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState, Fragment } from "react";
 import styled from "styled-components/native";
 import { WebView } from "react-native-webview";
 import * as Linking from "expo-linking";
-
+import Alert from "@UI/Alert";
 export const ExtendedWebView = (props) => {
   const { uri, onLoadStart, ...restProps } = props;
   const [currentURI, setURI] = useState(props.source.uri);
   const newSource = { ...props.source, uri: currentURI };
+  const [alert, setAlert] = useState();
   const onMessage = (event) => {
     // console.log(obj.nativeEvent.data);
     const message = JSON.parse(event.nativeEvent.data);
@@ -15,6 +16,12 @@ export const ExtendedWebView = (props) => {
       case "openURL":
         return Linking.openURL(message.url);
       case "alert":
+        setAlert({
+          message: message.message,
+          onPressConfirm: () => {
+            setAlert(null);
+          },
+        });
         return;
     }
   };
@@ -22,31 +29,44 @@ export const ExtendedWebView = (props) => {
     // console.warn(newNavState);
   };
   return (
-    <WebView
-      {...restProps}
-      // source={newSource}
-      style={[{ opacity: 0.99 }, props.style]}
-      scalesPageToFit={true}
-      originWhitelist={["*"]}
-      allowFileAccess={true}
-      domStorageEnabled={true}
-      javaScriptEnabled={true}
-      allowUniversalAccessFromFileURLs={true}
-      allowFileAccessFromFileURLs={true}
-      mixedContentMode="always"
-      sharedCookiesEnabled={true}
-      onMessage={(event) => onMessage(event)}
-      // onShouldStartLoadWithRequest={(request) => {
-      //   // console.warn(request.url);
-      //   // If we're loading the current URI, allow it to load
-      //   if (request.url === currentURI) return true;
-      //   // We're loading a new URL -- change state first
-      //   setURI(request.url);
-      //   return false;
-      // }}
-      onNavigationStateChange={onNavigationStateChange}
-      startInLoadingState={true}
-      scalesPageToFit={true}
-    />
+    <>
+      {alert && (
+        <Alert
+          isVisible={alert.content || alert.message ? true : false}
+          message={alert.message}
+          onPressConfirm={alert.onPressConfirm}
+          onPressCancel={alert.onPressCancel}
+          cancelText={alert.cancelText}
+          confirmText={alert.confirmText}
+          content={alert.content}
+        />
+      )}
+      <WebView
+        {...restProps}
+        // source={newSource}
+        style={[{ opacity: 0.99 }, props.style]}
+        scalesPageToFit={true}
+        originWhitelist={["*"]}
+        allowFileAccess={true}
+        domStorageEnabled={true}
+        javaScriptEnabled={true}
+        allowUniversalAccessFromFileURLs={true}
+        allowFileAccessFromFileURLs={true}
+        mixedContentMode="always"
+        sharedCookiesEnabled={true}
+        onMessage={(event) => onMessage(event)}
+        // onShouldStartLoadWithRequest={(request) => {
+        //   // console.warn(request.url);
+        //   // If we're loading the current URI, allow it to load
+        //   if (request.url === currentURI) return true;
+        //   // We're loading a new URL -- change state first
+        //   setURI(request.url);
+        //   return false;
+        // }}
+        onNavigationStateChange={onNavigationStateChange}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+      />
+    </>
   );
 };
