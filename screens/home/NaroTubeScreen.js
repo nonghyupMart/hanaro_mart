2aaef26dab6e45842db2ae5e6d847b4f7a69be22
Dominic <xodnghdud109@gmail.com>
2020-10-23 17:30:48 +0900

    나로튜브 새로고침

--- screens/home/NaroTubeScreen.js
+++ screens/home/NaroTubeScreen.js
@@ -9,9 +9,16 @@ import { useSelector, useDispatch } from "react-redux";
 import BaseScreen from "@components/BaseScreen";
 import _ from "lodash";
 const NaroTubeScreen = (props) => {
+  const navigation = props.navigation;
   const userStore = useSelector((state) => state.auth.userStore);
   const [url, setUrl] = useState();
-
+  const [key, setKey] = useState();
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      setKey(Math.random());
+    });
+    return unsubscribe;
+  }, []);
   useEffect(() => {
     let stringifyUrl;
     stringifyUrl = queryString.stringifyUrl({
@@ -21,6 +28,7 @@ const NaroTubeScreen = (props) => {
   }, []);
   return (
     <BaseScreen
+      key={key}
       style={styles.screen}
       isScroll={false}
       // isBottomNavigation={false}
