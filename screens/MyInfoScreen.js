8cc69759a544a2ec3986b1d5e6a4057ff6f96341
Dominic <xodnghdud109@gmail.com>
2020-11-11 17:34:55 +0900

    [앱취약성]부적절한 이용자 인증 허용 여부 - 타사용자 회원번호 확인

--- screens/MyInfoScreen.js
+++ screens/MyInfoScreen.js
@@ -35,7 +35,7 @@ const MyInfoScreen = (props) => {
     if (!_.isEmpty(userInfo)) {
       const userID = userInfo.user_id;
       const LastNumber = userID.substr(userID.length - 4);
-      const userCD = Util.pad(8, userInfo.user_cd);
+      const userCD = Util.pad(8, Util.decrypt(userInfo.user_cd));
       setBarcode(LastNumber + userCD);
     }
   }, [userInfo]);
