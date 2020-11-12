0aeecfbe03580fd7c74a3008a77d209cb3b87044
Dominic <xodnghdud109@gmail.com>
2020-11-12 15:07:42 +0900

    암호화되지 않은 user_cd 암호화 작업 취소, API에서 처리

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -80,9 +80,6 @@ export const saveUserStore = (userStore) => {
 };
 
 export const updateLoginLog = (query) => {
-  if (query.user_cd && !isNaN(query.user_cd)) {
-    query.user_cd = Util.encrypt(`${query.user_cd}`);
-  }
   const url = queryString.stringifyUrl({
     url: `${API_URL}/users`,
     query: query,
