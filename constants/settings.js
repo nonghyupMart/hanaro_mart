b0949cff11a3a4a79d9837fd77a8459019b6d70e
Dominic <xodnghdud109@gmail.com>
2020-11-29 16:56:29 +0900

    앱 내부버전 변경 1.1.1

--- constants/settings.js
+++ constants/settings.js
@@ -8,7 +8,7 @@ export const { SERVER_URL } = getEnvVars();
 export const API_URL = `${SERVER_URL}/api`;
 export const IMAGE_URL = `${SERVER_URL}`;
 
-export const INTERNAL_APP_VERSION = "1.0.13.1"
+export const INTERNAL_APP_VERSION = "1.1.1"
 
 /*
 링크 카테고리
