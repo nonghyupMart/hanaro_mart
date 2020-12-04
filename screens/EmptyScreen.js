a3def8feba705df59f0beb9fea8899437379fd60
Dominic <xodnghdud109@gmail.com>
2020-12-04 19:15:03 +0900

    인증되지 않은 회원이 메뉴 선택시 문구 변경 : 회원가입 -> 휴대폰인증

--- screens/EmptyScreen.js
+++ screens/EmptyScreen.js
@@ -84,8 +84,8 @@ const EmptyScreen = (props) => {
       } else {
         dispatch(
           setAlert({
-            message: "회원가입후 사용하실 수\n있는 메뉴입니다.",
-            confirmText: "회원가입",
+            message: "휴대폰인증후 사용하실 수\n있는 메뉴입니다.",
+            confirmText: "휴대폰인증",
             onPressConfirm: () => {
               dispatch(setAlert(null));
               dispatch(setPreview(false));
