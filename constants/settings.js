26f01dc80df28894f9ad219979953ccc2a63aa61
Dominic <xodnghdud109@gmail.com>
2020-12-10 10:02:30 +0900

    1.2.2

--- constants/settings.js
+++ constants/settings.js
@@ -8,7 +8,7 @@ export const { SERVER_URL } = getEnvVars();
 export const API_URL = `${SERVER_URL}/api`;
 export const IMAGE_URL = `${SERVER_URL}`;
 
-export const INTERNAL_APP_VERSION = "1.2.1";
+export const INTERNAL_APP_VERSION = "1.2.2";
 
 /*
 링크 카테고리
