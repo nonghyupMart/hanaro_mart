3389dbf7b0773ea090cc8804eeffb123a1d464ff
Dominic <xodnghdud109@gmail.com>
2021-03-02 18:06:30 +0900

    강제 업데이트 화면 로딩 인디케이터 숨김

--- screens/StartupScreen.js
+++ screens/StartupScreen.js
@@ -39,6 +39,7 @@ const StartupScreen = (props) => {
         if (versionCheck < 0) {
           //버전이 낮을때만 업데이트 팝업 페이지로 이동
           dispatch(CommonActions.setIsUpdated(false));
+          dispatch(CommonActions.setIsLoading(false));
           return;
         }
       });
