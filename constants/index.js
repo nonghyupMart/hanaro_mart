dd04b01c1e16060090cd1ebac6ee8a40aa9ebb26
Dominic <xodnghdud109@gmail.com>
2021-03-05 10:47:17 +0900

    1.7.7

--- constants/index.js
+++ constants/index.js
@@ -8,7 +8,7 @@ export const { SERVER_URL } = getEnvVars();
 export const API_URL = `${SERVER_URL}/api`;
 export const IMAGE_URL = `${SERVER_URL}`;
 
-export const INTERNAL_APP_VERSION = "1.7.6";
+export const INTERNAL_APP_VERSION = "1.7.7";
 
 export const PADDING_BOTTOM_MENU = 50;
 
