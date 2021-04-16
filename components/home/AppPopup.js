ca2363c323ea7f6a377eddd7c246efdcb31ae5cf
Dominic <xodnghdud109@gmail.com>
2021-04-16 15:48:53 +0900

    [통합관리지 팝업] 이벤트 페이지로 링크 기능 추가

--- components/home/AppPopup.js
+++ components/home/AppPopup.js
@@ -97,7 +97,10 @@ const AppPopup = (props) => {
                 activeOpacity={0.8}
                 key={item.pop_cd}
                 onPress={() => {
-                  if (item.link_url != "") Linking.openURL(item.link_url);
+                  if (item.link_url) Linking.openURL(item.link_url);
+                  else if (item.link_gbn) {
+                    dispatch(CommonActions.setDidTryPopup(item));
+                  }
                 }}
               >
                 <BaseImage
