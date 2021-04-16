b700c3a6814570bf350eee7494c86c2079af05ca
Dominic <xodnghdud109@gmail.com>
2021-04-16 10:05:51 +0900

    안드로이드 스캔 카메라 비율 이상 수정

--- screens/BarCodeScannerScreen.js
+++ screens/BarCodeScannerScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components/native";
 import {
   Text,
@@ -8,6 +8,7 @@ import {
   Image,
   View,
   ImageBackground,
+  Platform,
 } from "react-native";
 import BaseScreen from "../components/BaseScreen";
 import { BackButton, TextTitle } from "../components/UI/header";
@@ -23,11 +24,15 @@ import {
 import * as CommonActions from "../store/actions/common";
 import { useSelector, useDispatch } from "react-redux";
 
+const DESIRED_RATIO = "16:9";
+
 const BarCodeScannerScreen = (props) => {
   const dispatch = useDispatch();
   const params = props.route.params;
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const camRef = useRef();
+  const [ratio, setRatio] = useState("16:9");
   useEffect(() => {
     dispatch(CommonActions.setBottomNavigation(false));
     return () => {
@@ -40,6 +45,19 @@ const BarCodeScannerScreen = (props) => {
       setHasPermission(status === "granted");
     })();
   }, []);
+
+  const prepareRatio = async () => {
+    if (Platform.OS === "android" && camRef.current) {
+      const ratios = await camRef.current.getSupportedRatiosAsync();
+      // See if the current device has your desired ratio, otherwise get the maximum supported one
+      // Usually the last element of "ratios" is the maximum supported ratio
+      const ratio =
+        ratios.find((ratio) => ratio === DESIRED_RATIO) ||
+        ratios[ratios.length - 1];
+
+      setState({ ratio });
+    }
+  };
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
@@ -73,6 +91,9 @@ const BarCodeScannerScreen = (props) => {
       }}
     >
       <Camera
+        onCameraReady={prepareRatio} // You can only get the supported ratios when the camera is mounted
+        ratio={ratio}
+        ref={(cam) => (camRef.current = cam)}
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         barCodeScannerSettings={{
           barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
