b008a1ddf61d490e3393ba580801c5973ce36961
Dominic <xodnghdud109@gmail.com>
2021-04-30 09:33:28 +0900

    [업데이트팝업] 로딩 인디케이터 계속 표시되는것 수정

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -326,7 +326,7 @@ export const fetchUpdate = () => {
   });
   return async (dispatch, getState) => {
     return http
-      .init(dispatch)
+      .init(dispatch, true)
       .get(url)
       .then(async (response) => {
         dispatch({
