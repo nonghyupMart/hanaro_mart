e243c723da9c92dde064a16d608f7f26d47bfacb
Dominic <xodnghdud109@gmail.com>
2021-08-04 10:50:35 +0900

    [Set] sentry/webpack-plugin

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,23 @@
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+
 module.exports = function (api) {
   api.cache(true);
   return {
+    devtool: "source-map",
     presets: ["babel-preset-expo"],
-    plugins: [["@babel/plugin-syntax-class-properties"]],
+    plugins: [
+      new SentryWebpackPlugin({
+        // sentry-cli configuration
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "hanaromart",
+        project: "hanaromart",
+        release: process.env.SENTRY_RELEASE,
+
+        // webpack-specific configuration
+        include: ".",
+        ignore: ["node_modules", "webpack.config.js", "babel.config.js"],
+      }),
+      ["@babel/plugin-syntax-class-properties"],
+    ],
   };
 };
