dd05c40f9b5fbeeeef5bbde2b28bcab0daa1ce66
Dominic <xodnghdud109@gmail.com>
2021-08-31 14:06:37 +0900

    [Optimized] imports in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
+import AppLoading from "expo-app-loading";
+import Constants from "expo-constants";
 import "expo-dev-client";
 import { usePreventScreenCapture } from "expo-screen-capture";
-import React, { useState, useEffect } from "react";
-import { Provider } from "react-redux";
+import React, { useState } from "react";
 import { StatusBar } from "react-native";
-import AppNavigator from "./navigation/AppNavigator";
-import AppLoading from "expo-app-loading";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AppearanceProvider } from "react-native-appearance";
-import Constants from "expo-constants";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
 import {
-  store,
-  fetchFonts,
-  initStatusBarStyle,
-  routingInstrumentation,
-  initializeApplication,
+  fetchFonts, initializeApplication, initStatusBarStyle,
+  routingInstrumentation, store
 } from "./helpers";
+import AppNavigator from "./navigation/AppNavigator";
 
 initializeApplication();
 
